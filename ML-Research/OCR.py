e1c9a1d0e6423d865d7609996b5d5b00196a04ae
Chandar S <Chandar_S@ad.infosys.com>
2019-02-03 09:38:25 +0530

    Signed-off-by: Chandar S

--- ML-Research/OCR.py
+++ ML-Research/OCR.py
@@ -6,11 +6,46 @@ Created on Tue Jan 22 09:58:53 2019
 """
 
 import cv2
-import pytesseract
+#import sys
+import numpy as np
+import copy
 
-img = cv2.imread("C:\\Users\\chandar_s\\Pictures\\TestSheet2.tif")
+#sys.path.insert(0, '..\ML-Research')
+from ReadLines import ReadLines
+
+readLineObj = ReadLines()
+img = cv2.imread("C:\\Users\\chandar_s\\Pictures\\TestSheet1.tif")
+
+def FindContour(img):
+    rgb = cv2.pyrDown(img)
+    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+    
+    
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+    # using RETR_EXTERNAL instead of RETR_CCOMP
+    im2, contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    mask = np.zeros(bw.shape, dtype=np.uint8)
+    
+    for idx in range(len(contours)):
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y+h, x:x+w] = 0
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+    
+        if r > 0.45 and w > 8 and h > 8:
+            cv2.rectangle(rgb, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+    
+    cv2.imshow('rects', rgb)
+    cv2.waitKey()
+
 height, width = img.shape[:2]
-
 w_start = 0
 h_start = 150
 h_step_size = int(height/5)
@@ -28,39 +63,78 @@ for w_current in range(w_start, width, w_step_size) :
         crop_img = img[h_current:h_current + h_step_size, 
                        w_current  :w_current   + w_step_size]
 
-#        cv2.imshow("cropped", crop_img)
-#        cv2.waitKey(0)
         cv2.imwrite( "TestSheet2_Section"+str(image_count) +".jpg", crop_img );
         
-        ''' BREAK BY ROWS - VERTICAL SUBSECTION '''
-        image_Vsection_count = 1
-        h_Vsubsection_start = 0
-        h_Vsubsection_step_size= int(h_step_size/6)
-        for h_Vsubsection_current in range(h_Vsubsection_start, h_Vsubsection_start + h_step_size, h_Vsubsection_step_size) :
-             crop_Vsection_img = crop_img[h_Vsubsection_current : h_Vsubsection_current + h_Vsubsection_step_size, 
-                               0                 : w_step_size]
-             
-#             cv2.imwrite( "TestSheet1_Section"+str(image_count) + "_VSubSection" + str(image_Vsection_count) + ".jpg", crop_Vsection_img );
+        # Find the vertical crop co-ordinates
+        img_subsection, img_subsection_withlines, lines = readLineObj.DetectEdgesAndLines(None, copy.deepcopy(crop_img))
+        vert_coords, return_area = readLineObj.Normalize_vertical_lines(img_subsection_withlines, lines)
+        
+        print (f"Vertical Areas --> {return_area}")
+        q75, q25 = np.percentile(return_area, [75 ,25])
+        minimum_vertical_area_threshold = q25 - (q75 - q25) * 1.5
+        minimum_vertical_area_threshold = 20000 if  minimum_vertical_area_threshold < 0 else minimum_vertical_area_threshold
+        print (f"minimum_vertical_area_threshold {minimum_vertical_area_threshold}")
+
+        print ("   ~~START VERTICAL AREA~~")
+        for x1, y1, x2, y2, area in vert_coords:
+            print (f'     Vertical split -> x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}, area:{area}')
+            if area < minimum_vertical_area_threshold:
+                print (f"    Area not meeting minimum threshold of {minimum_vertical_area_threshold}. Skipping..")
+                continue
+            cropped_vertical_image = crop_img[y1: y2, x1:x2]
+            # Find the horizontal crop co-ordinates
+            img_subsection, img_subsection_withlines, lines = readLineObj.DetectEdgesAndLines(None, copy.deepcopy(cropped_vertical_image))
+
+#            cv2.imshow('rects', cropped_vertical_image)
+#            cv2.waitKey()
+
+            print ("  ~~START HORIZONTAL AREA~~")
             
-             ''' BREAK BY COLUMNS - HORIZONDAL SUBSECTION '''
-             image_Hsection_count = 1
-             h_Hsubsection_start = 50
-             print (crop_Vsection_img.shape)
-             ''' MAKE A SQUARE IMAGE BY MATCHING HEIGHT TO WIDTH'''
-             h_Hsubsection_step_size= 70 #crop_Vsection_img.shape[0]
-             for h_Hsubsection_current in range(h_Hsubsection_start, h_Hsubsection_start + w_step_size, h_Hsubsection_step_size) :
-                 crop_Hsection_img = crop_Vsection_img[40 : 40 + h_Hsubsection_step_size, 
-                                              h_Hsubsection_current : h_Hsubsection_current + h_Hsubsection_step_size]
-             
-#                 cv2.imwrite( "TestSheet1_Section"+str(image_count) + "_VSubSection" + str(image_Vsection_count) + "_HSubSection" + str(image_Hsection_count) +".jpg", crop_Hsection_img )
-                 image_Hsection_count += 1
-            
-             image_Vsection_count += 1
-             break
-        image_count += 1;
-#        break
-        # Run tesseract OCR on image
-#       text = pytesseract.image_to_string(crop_img, config=config)
-        # Print recognized text
-#       print(text)
-    break   
+            # proceed if basic lines are detected
+            if not lines is None:
+                horiz_coords, return_area = readLineObj.Normalize_horizontal_lines(img_subsection_withlines, lines)
+                print (f"    Horizondal Areas --> {return_area}")
+                q75, q25 = np.percentile(return_area, [75 ,25])
+                minimum_horizontal_area_threshold = q25 - (q75 - q25) * 1.5
+                print (f"    minimum_horizontal_area_threshold {minimum_horizontal_area_threshold}")
+                for x1, y1, x2, y2, area in horiz_coords:
+                    if area == [] or area < minimum_horizontal_area_threshold:
+                        print (f"     Area not meeting minimum threshold of {minimum_horizontal_area_threshold}. Skipping..")
+                        continue
+                    print (f'    Horizontal split -> x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}, area:{area}')
+                    cropped_horizondal_image = cropped_vertical_image[y1: y2 + 10, x1:x2]
+                    FindContour(cropped_horizondal_image)
+            else:
+                print("    No Horizontal lines detected. Skipping...")
+        break
+    break
+
+
+
+#      ''' BREAK BY ROWS - VERTICAL SUBSECTION '''
+#        image_Vsection_count = 1
+#        h_Vsubsection_start = 0
+#        h_Vsubsection_step_size= int(h_step_size/6)
+#        for h_Vsubsection_current in range(h_Vsubsection_start, h_Vsubsection_start + h_step_size, h_Vsubsection_step_size) :
+#             crop_Vsection_img = crop_img[h_Vsubsection_current : h_Vsubsection_current + h_Vsubsection_step_size, 
+#                               0                 : w_step_size]
+#             
+##             cv2.imwrite( "TestSheet1_Section"+str(image_count) + "_VSubSection" + str(image_Vsection_count) + ".jpg", crop_Vsection_img );
+#            
+#             ''' BREAK BY COLUMNS - HORIZONDAL SUBSECTION '''
+#             image_Hsection_count = 1
+#             h_Hsubsection_start = 50
+#             print (crop_Vsection_img.shape)
+#             ''' MAKE A SQUARE IMAGE BY MATCHING HEIGHT TO WIDTH'''
+#             h_Hsubsection_step_size= 70 #crop_Vsection_img.shape[0]
+#             for h_Hsubsection_current in range(h_Hsubsection_start, h_Hsubsection_start + w_step_size, h_Hsubsection_step_size) :
+#                 crop_Hsection_img = crop_Vsection_img[40 : 40 + h_Hsubsection_step_size, 
+#                                              h_Hsubsection_current : h_Hsubsection_current + h_Hsubsection_step_size]
+#             
+##                 cv2.imwrite( "TestSheet1_Section"+str(image_count) + "_VSubSection" + str(image_Vsection_count) + "_HSubSection" + str(image_Hsection_count) +".jpg", crop_Hsection_img )
+#                 image_Hsection_count += 1
+#            
+#             image_Vsection_count += 1
+#             break
+#        image_count += 1;
+##        break
--- ML-Research/ReadLines.py
+++ ML-Research/ReadLines.py
@@ -26,9 +26,10 @@ class ReadLines:
         img_org = copy.deepcopy(img)
 
         ## Draw initial detected lines and then finetune
-        for line in lines:
-            x1,y1,x2,y2 = line[0]
-            cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
+        if not lines is None:
+            for line in lines:
+                x1,y1,x2,y2 = line[0]
+                cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
         
         return img, img_org, lines
     
@@ -36,73 +37,180 @@ class ReadLines:
     def Normalize_horizontal_lines(self, img_org, lines):
 
         ''' Hyper Parameters'''
-        h_step = 3
+        v_step = 3
         #Line_count_merge_threshold = 7
         Line_count_merge_threshold = 5
-        #minimum_height_threshold_default = 20 
-        minimum_height_threshold_default = 20
+        ## what should be the minimum distance between 2 lines which 
+        #minimum_distance_threshold_default = 20 
+        minimum_distance_threshold_default = 20
+        ## what should be the average length of the line which should be considered as a true line
         #minimum_length_threshold_default = 10
         minimum_length_threshold_default = 10
 
         new_coord = []
-        distance_array = []
-        
+        length_array = []
+        return_coord = []
+        return_area = []
+       
         # see merging of lines along x axis as we convolve in Y axis
-        for current_height in range (0, img_org.shape[0], h_step):
+        for current_height in range (0, img_org.shape[0], v_step):
             new_x1 = -1
             new_x2 = -1
-            new_y = current_height + int(h_step/2)
-            distance = 0
+            new_y = current_height + int(v_step/2)
+            length = 0
             lines_merged = 0
             ## SCAN EACH LINE AND SEE IF IT CAN BE MERGED
             for line in lines:
                 x1,y1,x2,y2 = line[0]
-                # CHECK IF FALLS INTO 
-                if (int(np.mean([y1, y2])) >= current_height and int(np.mean([y1, y2])) < current_height + h_step):
+                # CHECK IF FALLS INTO HORIZONTAL RANGE
+                if (int(np.mean([y1, y2])) >= current_height and int(np.mean([y1, y2])) < current_height + v_step):
 #                    new_x1 = min(x1, x2) if (new_x1 == -1) else min(new_x1, x1, x2)
 #                    new_x2 = max(new_x1, x1, x2)
-                    distance += np.sqrt(np.square(x2 - x1) + np.square(y2 - y1))
+                    length += np.sqrt(np.square(x2 - x1))
                     new_x1 = 1
                     new_x2 = img_org.shape[1]
                     lines_merged += 1
             
             # This confirms merging happened
             if (new_x1 > 0):
-                distance_array.append(int(distance/lines_merged))
-                new_coord.append([new_x1, new_y, new_x2, new_y, lines_merged, int(distance/lines_merged)]) if lines_merged > Line_count_merge_threshold else None
+                length_array.append(int(length/lines_merged))
+                new_coord.append([new_x1, new_y, new_x2, new_y, lines_merged, int(length/lines_merged)]) if lines_merged > Line_count_merge_threshold else None
         #        new_coord.append([new_x1, new_y, new_x2, new_y, lines_merged, int(distance/lines_merged)])
                 
         # Find minimun outlier based on Interquartile Range and Outliers model
-        q75, q25 = np.percentile(distance_array, [75 ,25])
-        minimum_length_threshold = q25 - (q75 - q25) * 1.5
+        if length_array != []:
+            q75, q25 = np.percentile(length_array, [75 ,25])
+            minimum_length_threshold = q25 - (q75 - q25) * 1.5
+        else:
+            minimum_length_threshold = -1
+        minimum_length_threshold= -1
         minimum_length_threshold = minimum_length_threshold_default if minimum_length_threshold < 0 else minimum_length_threshold
         
         prev_y = 0
-        height = 0
-        #for x1,y1,x2,y2, avg_dist in new_coord:
-        
+        distance = 0
         updated_new_coord = []
-        height_array = []
+        distance_array = []
         
+        ## MERGE NEAR BY LINES TO MAKE IT 1 HORIZAONTAL LINE
         for rec in new_coord:
             x1, y1, x2, y2, lines_merged, avg_length = rec
-            height = np.abs(y1 - prev_y)
+            distance = minimum_distance_threshold_default + 1 if prev_y == 0 else np.abs(y1 - prev_y)
             prev_y = y1
-            height_array.append(height)
-            updated_new_coord.append([x1, y1, x2, y2, lines_merged, avg_length, height])
+            distance_array.append(distance)
+            updated_new_coord.append([x1, y1, x2, y2, lines_merged, avg_length, distance])
         
         
         # Find minimun outlier based on Interquartile Range and Outliers model
-        q75, q25 = np.percentile(height_array, [75 ,25])
-        minimum_height_threshold = q25 - (q75 - q25) * 1.5
-        minimum_height_threshold = minimum_height_threshold_default if minimum_height_threshold < 0 else minimum_height_threshold
+        if distance_array != []:
+            q75, q25 = np.percentile(distance_array, [75 ,25])
+            minimum_distance_threshold = q25 - (q75 - q25) * 1.5
+        else:
+            minimum_distance_threshold = -1
+        minimum_distance_threshold = minimum_distance_threshold_default if minimum_distance_threshold <= 0 else minimum_distance_threshold
         
+        prev_coord = {"x1":0, "y1":0, "x2":img_org.shape[1], "y2":0}
         # loop through to plot
-        for x1, y1, x2, y2, lines_merged, avg_length, height in updated_new_coord:
-        #    cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2) if avg_length > minimum_length_threshold and height > minimum_length_threshold else None
+        for x1, y1, x2, y2, lines_merged, avg_length, distance in updated_new_coord:
+        #    cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2) if avg_length > minimum_length_threshold and distance > minimum_distance_threshold else None
         #    cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2)
-            cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2) if height > minimum_length_threshold else None
-               
+            if distance > minimum_distance_threshold:
+                  cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2) 
+                  area = np.abs(y2 - prev_coord["y1"]) * np.abs(x2 - x1)
+                  return_area.append(area)
+                  return_coord.append([x1,prev_coord["y1"],x2,y2,area])
+                  prev_coord = {"x1":x1,"y1":y1,"x2":x2,"y2":y2}
+
+        # Account for remaining space                
+        area = np.abs(img_org.shape[0] - prev_coord["y1"]) * np.abs(prev_coord["x2"] - prev_coord["x1"])
+        return_coord.append([prev_coord["x1"],prev_coord["y1"],prev_coord["x2"],img_org.shape[0],area])
+        return_area.append(area)
+
+        return return_coord, return_area
+    
+    ''' NORMALIZE THE LINES DETECTED - MERGE CLOSER LINES AND DRAW ONE SINGLE HORIZONTAL LINE INSTEAD OF MULTIPLE SMALL LINES'''
+    def Normalize_vertical_lines(self, img_org, lines):
+
+        ''' Hyper Parameters'''
+        h_step = 3
+        #Line_count_merge_threshold = 7
+        Line_count_merge_threshold = 3
+        ## what should be the minimum distance between 2 lines which 
+        #minimum_distance_threshold_default = 20 
+        minimum_distance_threshold_default = 20
+        ## what should be the average length of the line which should be considered as a true line
+        #minimum_length_threshold_default = 10
+        minimum_length_threshold_default = 15
+
+        new_coord = []
+        length_array = []
+        return_coord = []
+        return_area = []
+        
+        # see merging of lines along x axis as we convolve in Y axis
+        for current_width in range (0, img_org.shape[1], h_step):
+            new_y1 = -1
+            new_y2 = -1
+            new_x = current_width + int(h_step/2)
+            length = 0
+            lines_merged = 0
+            ## SCAN EACH LINE AND SEE IF IT CAN BE MERGED
+            for line in lines:
+                x1,y1,x2,y2 = line[0]
+                # CHECK IF FALLS INTO HORIZONTAL RANGE
+                if (int(np.abs(np.mean([x1, x2]))) >= current_width and int(np.abs(np.mean([x1, x2]))) < current_width + h_step):
+                    length += np.sqrt(np.square(y2 - y1))
+                    new_y1 = 1
+                    new_y2 = img_org.shape[0]
+                    lines_merged += 1
+            
+            # This confirms merging happened
+            if (new_y1 > 0):
+                length_array.append(int(length/lines_merged))
+                new_coord.append([new_x, new_y1, new_x, new_y2, lines_merged, int(length/lines_merged)]) if lines_merged > Line_count_merge_threshold else None
+#                new_coord.append([new_x, new_y1, new_x, new_y2, lines_merged, int(length/lines_merged)])
+                
+        # Find minimun outlier based on Interquartile Range and Outliers model
+        q75, q25 = np.percentile(length_array, [75 ,25])
+        minimum_length_threshold = q25 - (q75 - q25) * 1.5
+        minimum_length_threshold = minimum_length_threshold_default if minimum_length_threshold <= 0 else minimum_length_threshold
+        
+        prev_x = 0
+        distance = 0
+        updated_new_coord = []
+        distance_array = []
+        
+        ## MERGE NEAR BY LINES TO MAKE IT 1 VERTICAL LINE
+        for rec in new_coord:
+            x1, y1, x2, y2, lines_merged, avg_length = rec
+            distance = minimum_distance_threshold_default + 1 if prev_x == 0 else np.abs(x1 - prev_x)
+            prev_x = x1
+            distance_array.append(distance)
+            updated_new_coord.append([x1, y1, x2, y2, lines_merged, avg_length, distance])
+        
+        
+        # Find minimun outlier based on Interquartile Range and Outliers model
+        q75, q25 = np.percentile(distance_array, [75 ,25])
+        minimum_distance_threshold = q25 - (q75 - q25) * 1.5
+        minimum_distance_threshold =  minimum_distance_threshold_default if  minimum_distance_threshold < 0 else  minimum_distance_threshold
+        
+        prev_coord = {"x1":0}
+        # loop through to plot
+        for x1, y1, x2, y2, lines_merged, avg_length, distance in updated_new_coord:
+            if avg_length > minimum_length_threshold and distance > minimum_distance_threshold:
+                cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2)
+                area = np.abs(y2 - y1) * np.abs(x2 - prev_coord["x1"])
+                return_area.append(area)
+                return_coord.append([prev_coord["x1"],y1,x2,y2,area])
+                prev_coord = {"x1":x1,"y1":y1,"x2":x2,"y2":y2}
+#            cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2)
+#            cv2.line(img_org,(x1,y1),(x2,y2),(255,0,0),2) if distance > minimum_distance_threshold else None
+
+        # Account for remaining space                
+        area = np.abs(prev_coord["y2"] - prev_coord["y1"]) * np.abs(img_org.shape[1] - prev_coord["x1"])
+        return_coord.append([prev_coord["x1"],prev_coord["y1"],img_org.shape[1],prev_coord["y2"] ,area])
+        return_area.append(area)
+
+        return  return_coord, return_area      
     
     '''' WRITE THE DATA TO OUTPUT IMAGE AND SHOW THE DETAILS IN PLOT'''    
     def Draw_plots(self, img, img_org):
@@ -114,7 +222,7 @@ class ReadLines:
         
         ax1.imshow(img_org, cmap=plt.cm.gray)
         ax1.axis('off')
-        ax1.set_title('Normalized Detection', fontsize=20)
+        ax1.set_title('Enhanced Detection', fontsize=20)
         
         ax2.imshow(img, cmap=plt.cm.gray)
         ax2.axis('off')
@@ -128,10 +236,12 @@ class ReadLines:
 if __name__ == "__main__":
     readLineObj = ReadLines()
     
-    img_array = ['TestSheet1_Section1.jpg', 'TestSheet1_Section2.jpg', 'TestSheet1_Section3.jpg','TestSheet1_Section4.jpg','TestSheet1_Section5.jpg',
-                 'TestSheet2_Section1.jpg', 'TestSheet2_Section2.jpg', 'TestSheet2_Section3.jpg','TestSheet2_Section4.jpg','TestSheet2_Section5.jpg']
+#    img_array = ['TestSheet1_Section1.jpg', 'TestSheet1_Section2.jpg', 'TestSheet1_Section3.jpg','TestSheet1_Section4.jpg','TestSheet1_Section5.jpg',
+#                 'TestSheet2_Section1.jpg', 'TestSheet2_Section2.jpg', 'TestSheet2_Section3.jpg','TestSheet2_Section4.jpg','TestSheet2_Section5.jpg']
+    img_array = ['TestSheet1_Section3.jpg']
     
     for img_name in img_array:
         img, img_org, lines = readLineObj.DetectEdgesAndLines(img_name, None)
-        readLineObj.Normalize_horizontal_lines(img_org, lines)
+        horiz_coords = readLineObj.Normalize_horizontal_lines(img_org, lines)
+#        vert_coords = readLineObj.Normalize_vertical_lines(img_org, lines)
         readLineObj.Draw_plots(img, img_org)
